96a443a022d9419be093f829a6f609b73edff2e1
Satori32 <Huga8989+githubf@gmail.com>
2024-10-14 17:09:55 +0900

    add main

--- ソース.cpp
+++ ソース.cpp
@@ -16,6 +16,20 @@ clock_t End(TimeTimer& In) {
 	In.E = clock();
 	return In.E - In.S;
 }
+clock_t Result(TimeTimer& In) {
+	return In.E - In.S;
+}
 clock_t Elapsed(TimeTimer& In) {
 	return clock() - In.S;
+}
+
+int main() {
+	TimeTimer S;
+
+	Start(S);
+	clock_t E = End(S);
+	clock_t R = Result(S);
+	clock_t EE = Elapsed(S);
+
+	return 0;
 }
